23bcec05ce932e9c57657f9c7c051b952748b751
lwsdevops <smith.w.lance@gmail.com>
2015-02-16 13:27:02 -0700

    first working tests

--- test/01-test_configReader.js
+++ test/01-test_configReader.js
@@ -8,18 +8,22 @@ var configReader = require('../lib/configReader.js')
   , assert = require('assert');
 
 describe('configReader', function() {
+
   it('should be null', function(done) {
     config = configReader.appConfig(null)
     assert.equal(config,null);
     done();
   });
+
   it('should find the config file', function(done) {
     config = configReader.appConfig('config/inspector.json');
     assert(JSON.stringify(config).length > 0, JSON.stringify(config));
     done();
   });
+
   it('should log list of file in suites dir', function(done) {
     suites = configReader.suites();
     done();
   });
+
 });
